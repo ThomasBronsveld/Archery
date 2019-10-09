81e69f7f95ada023f5544381a2bc0835e95f46f6
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2019-10-09 15:38:39 +0200

    Initial push

---
+++ src/test/java/nl/hva/ict/se/ads/ExtendedArcherTest.java
@@ -0,0 +1,24 @@
+package nl.hva.ict.se.ads;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Place all your own tests for Archer in this class. Tests in any other class will be ignored!
+ */
+public class ExtendedArcherTest extends ArcherTest {
+
+    /**
+     * This test is to make sure an id once set cannot be changed.
+     */
+    @Test
+    public void checkIdChange() throws Exception {
+        List<Archer> archers = Archer.generateArchers(5);
+        assertThrows(Exception.class, () -> {
+            archers.get(4).setId(3);
+        });
+    }
+}
